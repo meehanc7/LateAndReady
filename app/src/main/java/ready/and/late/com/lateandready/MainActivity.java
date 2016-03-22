c8d1003e2d42d2636d8a16a5946d40112b12e42c
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-03-22 21:43:52 +0000

    Added SnackBar Method

--- app/src/main/java/ready/and/late/com/lateandready/MainActivity.java
+++ app/src/main/java/ready/and/late/com/lateandready/MainActivity.java
@@ -22,10 +22,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showSnackBar(view);
             }
         });
+    }
+
+    public void showSnackBar(View view){
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
     }
 
     @Override
