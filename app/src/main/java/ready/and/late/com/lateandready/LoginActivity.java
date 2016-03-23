459b48cf33ea8005ac45ed3002740e719590ba99
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-03-23 19:32:53 +0000

    Deleted MainActivity xml file and java. Replaced with Search activity xml and Java. Button should bring to new page. Won't run on phone.

--- app/src/main/java/ready/and/late/com/lateandready/LoginActivity.java
+++ app/src/main/java/ready/and/late/com/lateandready/LoginActivity.java
@@ -1,11 +1,13 @@
 package ready.and.late.com.lateandready;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,15 +15,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button advanceToSearch = (Button) findViewById(R.id.buttonLogin);
+        advanceToSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(LoginActivity.this, SearchActivity.class);
+                startActivity(intent);
+
             }
         });
     }
