f4c0cf2a331bd5b4ec1b07e26d77f1eb8e3d3aac
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-03-29 21:04:35 +0100

    clean up layouts and fix missing activities from manifest

--- app/src/main/java/ready/and/late/com/lateandready/SearchActivity.java
+++ app/src/main/java/ready/and/late/com/lateandready/SearchActivity.java
@@ -1,24 +1,22 @@
 package ready.and.late.com.lateandready;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-
+        setContentView(R.layout.activity_search_);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
     }
 
 }
-
