1f97f7fa02e02cc2c987ddcc23dd5d35f36557c9
markf <mark.finnerty5@mail.dcu.ie>
2016-04-05 15:16:46 +0100

    Congratulations page designed - hardcoded

---
+++ app/src/main/java/ready/and/late/com/lateandready/CongratsActivity.java
@@ -0,0 +1,14 @@
+package ready.and.late.com.lateandready;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CongratsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrats);
+    }
+
+}
