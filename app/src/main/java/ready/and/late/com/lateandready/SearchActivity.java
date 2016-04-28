4b2c805b0949020500b52725a2aa7b9149d8ab39
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-04-28 21:49:50 +0100

    Added Date and No of passengers

--- app/src/main/java/ready/and/late/com/lateandready/SearchActivity.java
+++ app/src/main/java/ready/and/late/com/lateandready/SearchActivity.java
@@ -1,17 +1,24 @@
 package ready.and.late.com.lateandready;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import ready.and.late.com.lateandready.helpers.SearchDestinationHelper;
 import ready.and.late.com.lateandready.helpers.SearchDestinationResultsInterface;
@@ -21,7 +28,9 @@ public class SearchActivity extends AppCompatActivity {
 
     private AutoCompleteTextView departureEditText;
     private AutoCompleteTextView destinationEditText;
-
+    private EditText departureDate;
+    private EditText noOfPassengers;
+    private DatePickerDialog datePickerDialog;
 
 
 
@@ -30,8 +39,12 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+
         departureEditText = (AutoCompleteTextView)findViewById(R.id.departureEditText);
         destinationEditText = (AutoCompleteTextView) findViewById(R.id.destinationEditText);
+        departureDate = (EditText)findViewById(R.id.departureDate);
+        noOfPassengers = (EditText)findViewById(R.id.noOfPassengers);
+
 
         final SearchDestinationHelper searchDestinationHelper = new SearchDestinationHelper();
         searchDestinationHelper.getAirports(new SearchDestinationResultsInterface() {
@@ -46,8 +59,31 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        Calendar newCalendar = Calendar.getInstance();
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
-        Button advancetoSearchResults = (Button) findViewById(R.id.buttonSearchFlight);
+        //set todays date
+        departureDate.setText(dateFormatter.format(new Date().getTime()));
+        departureDate.setInputType(InputType.TYPE_NULL);
+        departureDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        //date picker when the user clicks the date
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                departureDate.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+
+                Button advancetoSearchResults = (Button) findViewById(R.id.buttonSearchFlight);
         advancetoSearchResults.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
