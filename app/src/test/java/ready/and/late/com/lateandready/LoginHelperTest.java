fc527a7158f5b84dceceb7883fba03de8093a81d
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-05-03 21:32:36 +0100

    create LoginHelperTest

---
+++ app/src/test/java/ready/and/late/com/lateandready/LoginHelperTest.java
@@ -0,0 +1,15 @@
+package ready.and.late.com.lateandready;
+
+import org.junit.Test;
+
+/**
+ * Created by OEM on 03-May-16.
+ */
+public class LoginHelperTest {
+
+
+    @Test
+    public void testLoginHelper() throws Exception {
+
+    }
+}
