e5fd898c376b5fb61664fdf7ec27d450530ad929
meehanc7 <conor.meehan7@mail.dcu.ie>
2016-05-03 21:53:48 +0100

    tested for when user enters the wrong login details, and for when they enter the right login details

--- app/src/test/java/ready/and/late/com/lateandready/LoginHelperTest.java
+++ app/src/test/java/ready/and/late/com/lateandready/LoginHelperTest.java
@@ -2,6 +2,12 @@ package ready.and.late.com.lateandready;
 
 import org.junit.Test;
 
+import ready.and.late.com.lateandready.helpers.LoginHelper;
+import ready.and.late.com.lateandready.helpers.LoginResultInterface;
+
+import static org.junit.Assert.*;
+
+
 /**
  * Created by OEM on 03-May-16.
  */
@@ -9,7 +15,47 @@ public class LoginHelperTest {
 
 
     @Test
-    public void testLoginHelper() throws Exception {
+    public void testLoginHelperWrongUsernamePassword() throws Exception {
+        LoginHelper loginHelper = new LoginHelper();
+
+
+        loginHelper.login("hello", "what", new LoginResultInterface() {
+            @Override
+            public void loginSuccesful() {
+
+                fail();
+            }
+
+            @Override
+            public void loginFailed(String errormessage) {
+                assertTrue(true);
+
+            }
+        });
+
+    }
+
+
+
+    @Test
+    public void testLoginHelperCorrectUsernamePassword() throws Exception {
+        LoginHelper loginHelper = new LoginHelper();
+
+
+        loginHelper.login("conor.meehan21@gmail.com", "rightpassword", new LoginResultInterface() {
+            @Override
+            public void loginSuccesful() {
+
+                assertTrue(true);
+            }
+
+            @Override
+            public void loginFailed(String errormessage) {
+
+                fail();
+
+            }
+        });
 
     }
 }
